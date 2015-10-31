dbd463673a035c6cc7ab8a338203d5e8c3ccdd3b
soynog <nicolas.o.garcia@gmail.com>
2015-10-31 14:50:03 -0400

    More object-oriented
    Updated aspect/blazon structure to be fully object-oriented.

--- DeckBuilder.rb
+++ DeckBuilder.rb
@@ -3,11 +3,102 @@
 # Heraldy Module includes info about tinctures, charges, etc.
 module Heraldry
 	# These hashes help translate the Blazon from numbers into descriptions of the Vassal aspects.
-	BLAZ = {0 => "Escutcheon", 1 => "Field A", 2 => "Field B", 3 => "Charge A", 4 => "Charge A Tincture", 5 => "Charge B", 6 => "Charge B Tincture"}
-	ESCT = {1 => "Iberian", 2 => "Swiss", 3 => "French"}
-	TINC = {0 => nil, 1 => "Argent", 2 => "Or", 3 => "Sable", 4 => "Gules", 5 => "Azure"}
-	CHRG = {0 => nil, 1 => "Crescent", 2 => "Fleur-de-Lis", 3 => "Cross", 4 => "Rose", 5 => "Lion", 6 => "Eagle"}
-	BLAZ_TYPE = [ESCT,TINC,TINC,CHRG,TINC,CHRG,TINC]
+	TINCTURES = ["Argent", "Or", "Sable", "Gules", "Azure"]
+	ESCUTCHEONS = ["Iberian", "Swiss", "French"]
+	CHARGES = ["Crescent", "Fleur-de-Lis", "Cross", "Rose", "Lion", "Eagle"]
+end
+
+# Defines a tincture, which is used to describe fields and charges.
+class Tincture
+	include Heraldry
+	def initialize(tinc)
+		@tinc = tinc if TINCTURES.include?(tinc)
+	end
+	
+	def to_s
+		@tinc
+	end
+	
+	def compare(other)
+		sim = 0
+		sim += 1 if @tinc == other.tinc
+		return sim
+	end
+	
+	attr_accessor :tinc
+end
+
+# Defines an Escutcheon, the shape of the coat of arms.
+class Escutcheon
+	include Heraldry
+	def initialize(esc)
+		@esc = esc if ESCUTCHEONS.include?(esc)
+	end
+	
+	def to_s
+		@esc
+	end
+	
+	def compare(other)
+		sim = 0
+		sim += 1 if @esc == other.esc
+		return sim
+	end
+	
+	attr_accessor :esc
+end
+
+# Defines a Field, the background of the coat of arms.
+class Field
+	include Heraldry
+	def initialize(*tincs)
+		@fields = []
+		tincs.each{|t| @fields.push(Tincture.new(t))}
+	end
+	
+	def add(*tincs)
+		tincs.each{|t| @fields.push(Tincture.new(t))}
+	end
+	
+	def split?
+		@fields.length > 1
+	end
+	
+	def to_s
+		@fields.join(", ")
+	end
+		
+	def compare(other)
+		sim = 0
+		@fields.each do |tA|
+			other.fields.each {|tB| sim += tA.compare(tB)}
+		end
+		return sim
+	end
+	
+	attr_accessor :fields
+end
+
+# Defines a Charge, a symbol which rests on the field and has a type and a tincture.
+class Charge
+	include Heraldry
+	def initialize(type, tinc)
+		@type = type if CHARGES.include?(type)
+		@tinc = Tincture.new(tinc)
+	end
+	
+	def to_s
+		@type + " " + @tinc.to_s
+	end
+	
+	def compare(other)
+		sim = 0
+		sim += 1 if @type == other.type
+		sim += @tinc.compare(other.tinc)
+		return sim
+	end
+	
+	attr_accessor :type, :tinc
 end
 
 # Defines a Vassal
@@ -15,35 +106,54 @@ class Vassal
 	include Heraldry
 	
 	# Initializes a new vassal
-	def initialize(name, blazon)
-		@name = name
-		@blazon = blazon
-		@esc = blazon[0]
-		@fldA = blazon[1]
-		@fldB = blazon[2]
-		@chgA = blazon[3]
-		@chgB = blazon[4]
-		@chgA_tinc = blazon[5]
-		@chgB_tinc = blazon[6]
+	def initialize(name, escutcheon, *blazon)
+		@name = name if name.is_a?(String)
+		@escutcheon = Escutcheon.new(escutcheon)
+		@field = Field.new()
+		@charges = []
+		
+		blazon.each_with_index do |arg, i|
+			if TINCTURES.include?(arg) && charges.length == 0
+				@field.add(arg)
+			elsif CHARGES.include?(arg)
+				@charges.push(Charge.new(arg,blazon[i+1]))
+			end
+		end
+		
+		# Arrays of unique charge attributes for comparison purposes:
+		@chg_types = @charges.collect {|c| c.type}.uniq
+		chg_tincs_str = @charges.collect {|c| c.tinc.to_s}.uniq
+		@chg_tincs = chg_tincs_str.collect {|c| Tincture.new(c)}
+	end
+
+	# Attribute Accessors for all instance variables (e.g. vassal.name, vassal.name=)
+	attr_accessor :name, :escutcheon, :field, :charges, :chg_types, :chg_tincs
+
+	def to_s
+		charge_str = @charges.collect {|c| c.to_s}
+		"Name: #{@name}\n" + 
+		"Escutcheon: #{@escutcheon.to_s}\n" + 
+		"Field: #{@field.to_s}\n" + 
+		"Charges: #{charge_str.join(", ")}\n"
 	end
 	
-	# Attribute Accessors for all instance variables (e.g. vassal.name, vassal.name=)
-	attr_accessor :name, :blazon, :esc, :fldA, :fldB, :chgA, :chgB, :chgA_tinc, :chgB_tinc
-
-	def inspect(style='t')
-		if style == 'v'
-			print "Name: #{@name}\n" + "Escutcheon: #{ESCT[@esc]}\n" + "Field: #{TINC[@fldA]} and #{TINC[@fldB]}\n" + "Charges: #{CHRG[@chgA]} #{TINC[@chgA_tinc]} and #{CHRG[@chgB]} #{TINC[@chgB_tinc]}\n"
-		elsif style == 't'
-			print "#{@name}"
-			@blazon.each{|v| print ",#{v}"}
-			print "\n"
-			nil
-		else
-			@name
+	def compare(other)
+		sim = 0
+		
+		sim += @escutcheon.compare(other.escutcheon)
+		sim	+= @field.compare(other.field)
+		
+		@chg_types.each do |typA|
+			other.chg_types.each {|typB| sim += 1 if typA == typB}
 		end
+		
+		@chg_tincs.each do |tncA|
+			other.chg_tincs.each {|tncB| sim += tncA.compare(tncB)}
+		end
+
+		return sim
 	end
 end
-
 
 # Defines a Deck of Vassals
 class Deck
@@ -55,22 +165,23 @@ class Deck
 	
 	attr_accessor :deck
 	
-	def append(aVassal)
+	# Adds a new vassal to the deck.
+	def add(aVassal)
 		@deck.push(aVassal)
 		self
-	end
-	
-	def [](key)
-		@deck[key]
-	end
-	
-	def inspect
-		@deck.each {|v| v.inspect('t')}
 	end
 	
 	# Deletes a vassal from the deck, by its location in the array
 	def remove(vassPos)
 		@deck.delete_at(vassPos)
+	end
+		
+	def [](key)
+		@deck[key]
+	end
+	
+	def to_s
+		@deck.each {|v| v.to_s}
 	end
 	
 	# Gets a Vassals's position in the array, given its name
@@ -148,19 +259,26 @@ end
 
 
 puts "MAKING VASSALS"
-#garcia = Vassal.new("Garcia",[1,1,2,1,2,3,4])
+garcia = Vassal.new("Garcia", "Iberian", "Argent", "Or", "Eagle", "Sable", "Lion", "Azure")
+vanW = Vassal.new("VanWeringh", "Iberian", "Argent", "Or", "Lion", "Sable", "Eagle", "Azure")
+puts garcia.to_s
+puts vanW.to_s
+puts garcia.compare(vanW)
+puts garcia.chg_types
+puts garcia.chg_tincs
+
 #vanW = Vassal.new("VanWeringh",[1,1,2,4,3,2,1])
 
-puts "MAKING DECK"
-deckTest = Deck.new
+#puts "MAKING DECK"
+#deckTest = Deck.new
 #deckTest.append(garcia)
 #deckTest.append(vanW)
 
-puts "FILLING DECK"
-deckTest.fill_up([])
+#puts "FILLING DECK"
+#deckTest.fill_up([])
 
-puts "SAVING DECK"
-deckTest.save
+#puts "SAVING DECK"
+#deckTest.save
 
 
 #deckTest.inspect
